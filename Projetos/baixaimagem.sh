ddaad5a9a5e50b31a1d0b3c9719eadd1a1440a9c
Jorge Lucas V Jabczenski <40120369+JorgeJabczenski@users.noreply.github.com>
2019-08-25 17:15:49 -0300

    Update baixaimagem.sh

--- Projetos/baixaimagem.sh
+++ Projetos/baixaimagem.sh
@@ -11,7 +11,6 @@ URL=$1
 NOMENOVO=$2
 wget $URL
 IMAGEM=$(grep og:image index.html | cut -d'"' -f4)
-echo "A URL DA IMAGEM EH $IMAGEM"
 wget $IMAGEM
 NOMEANTIGO=$(grep og:image index.html| cut -d'"' -f4 | cut -d/ -f9)
 rm index.html
