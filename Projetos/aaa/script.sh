099201f9581cf1c7ef3840559987d14678cabf00
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2019-09-13 01:48:14 -0300

    PRONTO

--- Projetos/aaa/script.sh
+++ Projetos/aaa/script.sh
@@ -5,23 +5,27 @@
 # Professor : Albini (Turma D)
 
 #################################################################################
-                                ##### ITEM 1 #####
+#                                     ITEM 1                                    #
 #################################################################################
-#Descompacta o arquivo
-tar -xzf evasao2014-18.tar.gz
+
+tar -xzf evasao2014-18.tar.gz      #Descompacta o arquivo
 
 #################################################################################
-                                ##### ITEM 2 #####
+#                                     ITEM 2                                    #
 #################################################################################
-#Vai para a pasta descompactada e junta todas as evasões em um arquivo só
-cd evasao
-cat evasao* > evasao-geral
+
+cd evasao                    # Vai para a pasta descompactada 
+cat evasao* > evasao-geral   # Junta todas as evasões em um arquivo só
+
 #################################################################################
-                                ##### ITEM 3 #####
+#                                     ITEM 3                                    #
 #################################################################################
-cat evasao-geral | cut -d, -f1 | sort -u > formas
-sed -i 's/FORMA_EVASAO//g' formas    # Tira o cabeçalho
-sed -i '/^$/d' formas                # Tira a linha em branco
+
+echo -e "[ITEM 3]"
+
+cat evasao-geral | cut -d',' -f1 | sort -u > formas # Acha todas as formas de evasão
+sed -i 's/FORMA_EVASAO//g' formas                   # Tira o cabeçalho
+sed -i '/^$/d' formas                               # Tira a linha em branco
 
 # Cria uma variavel FORMAS com todas as formas de desistência
 mapfile -t FORMAS < formas
@@ -33,22 +37,16 @@ do
     echo "${i}, ${j}" >> ranking-geral   
 done
 
-echo -e "[ITEM 3]"
-
 # Rankeia os tipos de evasão gerais
 awk '{print $NF,$0}' ranking-geral | sort -nr | cut -f2- -d' ' > ranking-geral-s
 echo -e "\nRANKING GERAL"
+
 column -t -s',' ranking-geral-s
-#cat ranking-geral-s
 
-#sort -t'/' -k2 -r -n -o ranking-geral ranking-geral
-#sed -i 's/\// /g' ranking-geral
-#echo -e "RANKING GERAL \n"
-#cat ranking-geral
+ls *.csv > nome-arquivos                 # Obtém os nomes de todos os arquivos que contém as evasões
+mapfile -t NOMEARQUIVOS < nome-arquivos  # Cria uma variável com esses nomes
 
-ls *.csv > nome-arquivos
-mapfile -t NOMEARQUIVOS < nome-arquivos
-
+# Faz a mesma coisa que o ranking geral, mas agora para todos os arquivos
 for i in ${NOMEARQUIVOS[@]}
 do
     for j in "${FORMAS[@]}"
@@ -60,12 +58,11 @@ do
     awk '{print $NF,$0}' $i- | sort -nr | cut -f2- -d' ' > $i-s
     echo -e "\nRANKING $i"
     column -t -s',' $i-s
-    #cat $i-s
     rm $i- 
-
 done
+
 #################################################################################
-                                ##### ITEM 4 #####
+#                                     ITEM 4                                    #
 #################################################################################
 
 echo -e "\n[ITEM 4]"
@@ -100,6 +97,7 @@ column -t permanencia.txt
 #################################################################################
 #                                   ITEM 5                                      #
 #################################################################################
+
 echo -e "\n[ITEM 5]"
 
 for i in ${NOMEARQUIVOS[@]}
@@ -121,11 +119,13 @@ do
     ano=$(echo $i | cut -d'-' -f2 | cut -d'.' -f1)
     echo "$ano semestre $sem - $pctg%" >> porcentagem-semestre
 done
+
 column -t porcentagem-semestre
 
 #################################################################################
 #                                   ITEM 6                                      #
 #################################################################################
+
 echo -e "\n[ITEM 6]\n"
 
 totalpessoas=$(cat evasao-geral | wc -l)  
@@ -141,3 +141,85 @@ echo "F,$pctgF%" >> porcentagem-sexo
 echo "M,$pctgM%" >> porcentagem-sexo
 
 column -t -s',' porcentagem-sexo
+
+#################################################################################
+#                                   ITEM 7                                      #
+#################################################################################
+
+for i in {2014..2018}
+do
+    NR_EVASOES=$(cat evasao-$i.csv | wc -l)
+    NR_EVASOES=$(expr $NR_EVASOES - 1)       #Tira a linha do cabeçalho
+    echo "$i $NR_EVASOES" >> grafico-nr-evasoes
+done
+
+gnuplot -persist <<-FimDeComando
+
+    set output 'evasoes-ano.png'
+    set terminal png
+    set key above
+    set title 'Nr° de Evasões por Ano'
+    set xlabel "Anos"
+    set ylabel "Nr° de Evasões"
+
+    plot 'grafico-nr-evasoes' with lines title 'Evasões'
+
+
+FimDeComando
+
+mv evasoes-ano.png ..
+
+#################################################################################
+#                                   ITEM 8                                      #
+#################################################################################
+
+for i in ${NOMEARQUIVOS[@]}
+do
+    cat $i | cut -d',' -f3 >> formas-ingresso     # Obtém todas as formas possíveis de ingresso
+done
+
+cat formas-ingresso | sort -u > formas-ingresso-s # Retira as formas repetidas
+sed -i 's/FORMA_INGRESSO//g' formas-ingresso-s    # Tira o cabeçalho
+sed -i '/^$/d' formas-ingresso-s                  # Tira a linha em branco
+
+mapfile -t FORMAS_INGRESSO < formas-ingresso-s    # Insere todas as formas em um vetor
+
+for i in {2014..2018}
+do
+    echo -n "$i " >> grafico-final
+    for j in "${FORMAS_INGRESSO[@]}" 
+    do
+        NR=$(grep -c "$j" evasao-$i.csv)
+        echo -n "$NR " >> grafico-final
+    done
+    echo >> grafico-final
+done
+
+gnuplot -persist <<-FimDeComando
+
+    set output 'evasoes-forma.png'
+    set terminal png
+    set title '[ITEM 8]'
+    set style data histogram
+    set style histogram cluster gap 1
+    set style fill solid
+    set boxwidth 0.8
+    set xtics format
+    set yrange[0:180]
+
+    plot 'grafico-final'\
+    using 2:xtic(1) title 'Aluno Intercâmbio',\
+    '' using 3         title 'Aproveitamento Curso Superior',\
+    '' using 4         title 'Convênio AUGM',\
+    '' using 5         title 'Convênio Pec-G',\
+    '' using 6         title 'Mobilidade Acadêmica',\
+    '' using 7         title 'Processo Seletivo/Enem',\
+    '' using 8         title 'Reopção',\
+    '' using 9         title 'Transferência Ex-Ofício',\
+    '' using 10        title 'Transferência Provar',\
+    '' using 11        title 'Vestibular',\
+
+
+FimDeComando
+
+mv evasoes-forma.png ..
